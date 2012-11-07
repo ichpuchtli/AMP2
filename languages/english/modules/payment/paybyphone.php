05107f7da8baee5325173d585194f9364dfe5bc8
Sam Macpherson <sam.mack91@gmail.com>
2012-11-07 15:39:23 +1000

    Added pay by phone support

---
+++ languages/english/modules/payment/paybyphone.php
@@ -0,0 +1,34 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003 The zen-cart developers                           |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+// $Id: PAYBYPHONE.php 1970 2011-01-29 01:36:21Z frank18 $
+//
+
+
+  define('MODULE_PAYMENT_PAYBYPHONE_TEXT_EMAIL_FOOTER', 
+  "Please call our office to pay for your total order value:\n\n" .
+  "\nPhone No.:  " . MODULE_PAYMENT_PAYBYPHONE_PHONENUM .
+  "\n\nThanks for your order which will ship immediately once we receive payment.\n");
+
+  define('MODULE_PAYMENT_PAYBYPHONE_TEXT_TITLE', 'Pay by Phone');
+   define('MODULE_PAYMENT_PAYBYPHONE_TEXT_DESCRIPTION', 
+  '<br />Please call our office to pay for your total order value:<br />' . 
+  "\nPhone No.:  " . MODULE_PAYMENT_PAYBYPHONE_PHONENUM .
+  '<p>Thanks for your order which will ship immediately once we receive payment.</p>');
+?>
