cc5ce65a867f147d78362f5d676df752e9714b74
Sam Macpherson <sam.mack91@gmail.com>
2012-11-21 17:15:47 +1000

    Cleaned up some poorly written PHP, unused .sql files removed

--- tempEP/index.php
+++ tempEP/index.php
@@ -91,12 +91,12 @@ Feel free to add, delete or alter these
 */
 
 $indexfiles = array (
-				'index.html',
-				'index.htm',
-				'default.htm',
-				'default.html'
-			);
-			
+                            'index.html',
+                            'index.htm',
+                            'default.htm',
+                            'default.html'
+                    );
+                    
 /*
 File Icons - If you want to add your own special file icons use 
 this section below. Each entry relates to the extension of the 
@@ -104,36 +104,36 @@ given file, in the form <extension> => <filename>.
 These files must be located within the dlf directory.
 */
 $filetypes = array (
-				'png' => 'jpg.gif',
-				'jpeg' => 'jpg.gif',
-				'bmp' => 'jpg.gif',
-				'jpg' => 'jpg.gif', 
-				'gif' => 'gif.gif',
-				'zip' => 'archive.png',
-				'rar' => 'archive.png',
-				'exe' => 'exe.gif',
-				'setup' => 'setup.gif',
-				'txt' => 'text.png',
-				'htm' => 'html.gif',
-				'html' => 'html.gif',
-				'fla' => 'fla.gif',
-				'swf' => 'swf.gif',
-				'xls' => 'xls.gif',
-				'doc' => 'doc.gif',
-				'sig' => 'sig.gif',
-				'fh10' => 'fh10.gif',
-				'pdf' => 'pdf.gif',
-				'psd' => 'psd.gif',
-				'rm' => 'real.gif',
-				'mpg' => 'video.gif',
-				'mpeg' => 'video.gif',
-				'mov' => 'video2.gif',
-				'avi' => 'video.gif',
-				'eps' => 'eps.gif',
-				'gz' => 'archive.png',
-				'asc' => 'sig.gif',
-			);
-			
+                            'png' => 'jpg.gif',
+                            'jpeg' => 'jpg.gif',
+                            'bmp' => 'jpg.gif',
+                            'jpg' => 'jpg.gif', 
+                            'gif' => 'gif.gif',
+                            'zip' => 'archive.png',
+                            'rar' => 'archive.png',
+                            'exe' => 'exe.gif',
+                            'setup' => 'setup.gif',
+                            'txt' => 'text.png',
+                            'htm' => 'html.gif',
+                            'html' => 'html.gif',
+                            'fla' => 'fla.gif',
+                            'swf' => 'swf.gif',
+                            'xls' => 'xls.gif',
+                            'doc' => 'doc.gif',
+                            'sig' => 'sig.gif',
+                            'fh10' => 'fh10.gif',
+                            'pdf' => 'pdf.gif',
+                            'psd' => 'psd.gif',
+                            'rm' => 'real.gif',
+                            'mpg' => 'video.gif',
+                            'mpeg' => 'video.gif',
+                            'mov' => 'video2.gif',
+                            'avi' => 'video.gif',
+                            'eps' => 'eps.gif',
+                            'gz' => 'archive.png',
+                            'asc' => 'sig.gif',
+                    );
+                    
 /*
 That's it! You are now ready to upload this script to the server.
 
@@ -148,128 +148,128 @@ if((substr($leadon, -1, 1)!='/') && $leadon!='') $leadon = $leadon . '/';
 $startdir = $leadon;
 
 if($_GET['dir']) {
-	//check this is okay.
-	
-	if(substr($_GET['dir'], -1, 1)!='/') {
-		$_GET['dir'] = $_GET['dir'] . '/';
-	}
-	
-	$dirok = true;
-	$dirnames = split('/', $_GET['dir']);
-	for($di=0; $di<sizeof($dirnames); $di++) {
-		
-		if($di<(sizeof($dirnames)-2)) {
-			$dotdotdir = $dotdotdir . $dirnames[$di] . '/';
-		}
-		
-		if($dirnames[$di] == '..') {
-			$dirok = false;
-		}
-	}
-	
-	if(substr($_GET['dir'], 0, 1)=='/') {
-		$dirok = false;
-	}
-	
-	if($dirok) {
-		 $leadon = $leadon . $_GET['dir'];
-	}
+    //check this is okay.
+    
+    if(substr($_GET['dir'], -1, 1)!='/') {
+            $_GET['dir'] = $_GET['dir'] . '/';
+    }
+    
+    $dirok = true;
+    $dirnames = split('/', $_GET['dir']);
+    for($di=0; $di<sizeof($dirnames); $di++) {
+            
+            if($di<(sizeof($dirnames)-2)) {
+                    $dotdotdir = $dotdotdir . $dirnames[$di] . '/';
+            }
+            
+            if($dirnames[$di] == '..') {
+                    $dirok = false;
+            }
+    }
+    
+    if(substr($_GET['dir'], 0, 1)=='/') {
+            $dirok = false;
+    }
+    
+    if($dirok) {
+             $leadon = $leadon . $_GET['dir'];
+    }
 }
 
 if($_GET['download'] && $forcedownloads) {
-	$file = str_replace('/', '', $_GET['download']);
-	$file = str_replace('..', '', $file);
+    $file = str_replace('/', '', $_GET['download']);
+    $file = str_replace('..', '', $file);
 
-	if(file_exists($leadon . $file)) {
-		header("Content-type: application/x-download");
-		header("Content-Length: ".filesize($leadon . $file)); 
-		header('Content-Disposition: attachment; filename="'.$file.'"');
-		readfile($leadon . $file);
-		die();
-	}
+    if(file_exists($leadon . $file)) {
+            header("Content-type: application/x-download");
+            header("Content-Length: ".filesize($leadon . $file)); 
+            header('Content-Disposition: attachment; filename="'.$file.'"');
+            readfile($leadon . $file);
+            die();
+    }
 }
 
 if($allowuploads && $_FILES['file']) {
-	$upload = true;
-	if(!$overwrite) {
-		if(file_exists($leadon.$_FILES['file']['name'])) {
-			$upload = false;
-		}
-	}
-	
-	if($upload) {
-		move_uploaded_file($_FILES['file']['tmp_name'], $leadon . $_FILES['file']['name']);
-	}
+    $upload = true;
+    if(!$overwrite) {
+            if(file_exists($leadon.$_FILES['file']['name'])) {
+                    $upload = false;
+            }
+    }
+    
+    if($upload) {
+            move_uploaded_file($_FILES['file']['tmp_name'], $leadon . $_FILES['file']['name']);
+    }
 }
 
 $opendir = $leadon;
 if(!$leadon) $opendir = '.';
 if(!file_exists($opendir)) {
-	$opendir = '.';
-	$leadon = $startdir;
+    $opendir = '.';
+    $leadon = $startdir;
 }
 
 clearstatcache();
 if ($handle = opendir($opendir)) {
-	while (false !== ($file = readdir($handle))) { 
-		//first see if this file is required in the listing
-		if ($file == "." || $file == "..")  continue;
-		$discard = false;
-		for($hi=0;$hi<sizeof($hide);$hi++) {
-			if(strpos($file, $hide[$hi])!==false) {
-				$discard = true;
-			}
-		}
-		
-		if($discard) continue;
-		if (@filetype($leadon.$file) == "dir") {
-			if(!$showdirs) continue;
-		
-			$n++;
-			if($_GET['sort']=="date") {
-				$key = @filemtime($leadon.$file) . ".$n";
-			}
-			else {
-				$key = $n;
-			}
-			$dirs[$key] = $file . "/";
-		}
-		else {
-			$n++;
-			if($_GET['sort']=="date") {
-				$key = @filemtime($leadon.$file) . ".$n";
-			}
-			elseif($_GET['sort']=="size") {
-				$key = @filesize($leadon.$file) . ".$n";
-			}
-			else {
-				$key = $n;
-			}
-			$files[$key] = $file;
-			
-			if($displayindex) {
-				if(in_array(strtolower($file), $indexfiles)) {
-					header("Location: $file");
-					die();
-				}
-			}
-		}
-	}
-	closedir($handle); 
+    while (false !== ($file = readdir($handle))) { 
+            //first see if this file is required in the listing
+            if ($file == "." || $file == "..")  continue;
+            $discard = false;
+            for($hi=0;$hi<sizeof($hide);$hi++) {
+                    if(strpos($file, $hide[$hi])!==false) {
+                            $discard = true;
+                    }
+            }
+            
+            if($discard) continue;
+            if (@filetype($leadon.$file) == "dir") {
+                    if(!$showdirs) continue;
+            
+                    $n++;
+                    if($_GET['sort']=="date") {
+                            $key = @filemtime($leadon.$file) . ".$n";
+                    }
+                    else {
+                            $key = $n;
+                    }
+                    $dirs[$key] = $file . "/";
+            }
+            else {
+                    $n++;
+                    if($_GET['sort']=="date") {
+                            $key = @filemtime($leadon.$file) . ".$n";
+                    }
+                    elseif($_GET['sort']=="size") {
+                            $key = @filesize($leadon.$file) . ".$n";
+                    }
+                    else {
+                            $key = $n;
+                    }
+                    $files[$key] = $file;
+                    
+                    if($displayindex) {
+                            if(in_array(strtolower($file), $indexfiles)) {
+                                    header("Location: $file");
+                                    die();
+                            }
+                    }
+            }
+    }
+    closedir($handle); 
 }
 
 //sort our files
 if($_GET['sort']=="date") {
-	@ksort($dirs, SORT_NUMERIC);
-	@ksort($files, SORT_NUMERIC);
+    @ksort($dirs, SORT_NUMERIC);
+    @ksort($files, SORT_NUMERIC);
 }
 elseif($_GET['sort']=="size") {
-	@natcasesort($dirs); 
-	@ksort($files, SORT_NUMERIC);
+    @natcasesort($dirs); 
+    @ksort($files, SORT_NUMERIC);
 }
 else {
-	@natcasesort($dirs); 
-	@natcasesort($files);
+    @natcasesort($dirs); 
+    @natcasesort($files);
 }
 
 //order correctly
